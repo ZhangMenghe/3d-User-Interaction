da497fab891f0beeeefd807786243db075edff71
ZhangMenghe <zmh.leaves@gmail.com>
2017-01-23 16:32:47 -0800

    Basic Component with meta

---
+++ BigWall/Assets/scripts/sceneController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneController : MonoBehaviour {
+    public float restartAngle = 0.9f;
+    public int restartTime = 10;
+    private int timer = 10;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Camera.main.transform.forward.y > restartAngle)
+        {
+            timer++;
+            if (timer > restartTime)
+                Application.LoadLevel("main");
+        }
+	}
+}
---
+++ BigWall/Assets/scripts/test.js
@@ -0,0 +1,10 @@
+﻿// JavaScript
+
+var brick : Transform;
+function Start () {
+    for (var y = 0; y < 5; y++) {
+        for (var x = 0; x < 5; x++) {
+            Instantiate(brick, Vector3 (x, y, 0), Quaternion.identity);
+        }
+    }
+}
